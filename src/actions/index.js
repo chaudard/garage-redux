cf2b5e1bccfe8cdbed913ca2edfc94fa7143c784
MICHEL Dany <donnetonfalzard+gitlab@gmail.com>
2019-07-18 10:32:08 +0200

    cars index : route , action creator, reducer and view

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
-// TODO: add and export your own actions
+const BASE_URL = 'https://wagon-garage-api.herokuapp.com';
+
+export function fetchCars(garage) {
+  const url = `${BASE_URL}/${garage}/cars`;
+  const promise = fetch(url)
+    .then(r => r.json());
+
+  return {
+    type: 'FETCH_CARS',
+    payload: promise // Will be resolved by redux-promise
+  };
+}
--- src/index.jsx
+++ src/index.jsx
@@ -7,22 +7,35 @@ import logger from 'redux-logger';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { createHistory as history } from 'history';
 
+import CarsIndex from './containers/cars_index';
 import '../assets/stylesheets/application.scss';
 
+import carsReducer from './reducers/cars_reducer.js';
+
+const garageName = prompt("What is your garage?") || `garage${Math.floor(10 + (Math.random() * 90))}`;
+const initialState = {
+  garage: garageName,
+  cars: []
+};
+
+const identityReducer = (state = null) => state;
 const reducers = combineReducers({
-  // key: reducer
+  garage: identityReducer,
+  cars: carsReducer
 });
 
 const middlewares = applyMiddleware(reduxPromise, logger);
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, {}, middlewares)}>
+  <Provider store={createStore(reducers, initialState, middlewares)}>
     <Router history={history}>
-      <Switch>
-        TODO
-      </Switch>
+      <div className="view-container">
+        <Switch>
+          <Route path="/" exact component={CarsIndex} />
+        </Switch>
+      </div>
     </Router>
   </Provider>,
-  document.getElementById('root')
+  document.querySelector('.container')
 );
